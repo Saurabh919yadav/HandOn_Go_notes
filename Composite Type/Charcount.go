18c69a25bf595ef64b12db11581b8875e4a0a8d3
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-04-03 19:43:48 +0530

    adding character count

---
+++ Composite Type/Charcount.go
@@ -0,0 +1,55 @@
+/*
+	Here is a program that contains the example of maps in action, a program that count the occurences of each distinct
+	Unicode code point in its point. Since there are a large number of possible characters, only a small fraction of
+	which would appear in any particular document, a map is a natural way to keep track of just the ones that have been
+	seen and their corresponding counts
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	counts := make(map[rune]int)
+	var utflen [utf8.UTFMax + 1]int
+	invalid := 0
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, " characount:%\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+	}
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\ncount")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+
+}
