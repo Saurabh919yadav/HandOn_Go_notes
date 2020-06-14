5af379cca4ebe83f77eb21e1cbf584058478e362
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-06-14 21:30:46 +0530

    Updating comments

--- echo/echo2.go
+++ echo/echo2.go
@@ -9,12 +9,12 @@ import (
 func main() {
 	s, sep := "", ""
 	/*
-	 In each iteration of the lop. range produces a pair of values: the index and the value of element at that index.
-	 Here we don't need index values, so a basic idea is to use a temporary variable lie temp and ignore its value,
-	 but Go does not permit unused local variales, so this would result in a ompilation error.
+		In each iteration of the loop. range produces a pair of values: the index and the value of element at that index.
+		Here we don't need index values, so a basic idea is to use a temporary variable lie temp and ignore its value,
+		but Go does not permit unused local variales, so this would result in a compilation error.
 
-	 The solution is to use blank identifier, whose name is _. the blank identifier may be used whenever syntax requires
-	 a variable name but program logic does not.
+		The solution is to use blank identifier, whose name is _. the blank identifier may be used whenever syntax requires
+		a variable name but program logic does not.
 	*/
 	for _, arg := range os.Args[1:] {
 		s += sep + arg
